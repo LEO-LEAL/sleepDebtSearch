2f627fb0aaabe34be89bd7089c4ac8596730b375
LEO-LEAL <leoleal97@gmail.com>
2016-08-09 14:16:03 -0700

    Testing Jedis

--- codeu-final/src/com/sleepdeptsearch/Searched.java
+++ codeu-final/src/com/sleepdeptsearch/Searched.java
@@ -3,10 +3,17 @@ package com.sleepdeptsearch;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import redis.clients.jedis.Jedis;
+
 @SuppressWarnings("serial")
 public class Searched extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Leo was here");
+		Jedis jedis= JedisMaker.make();
+		jedis.set("mykey", "myvalue");
+		String value = jedis.get("mykey");
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Got value: " + value);
 	}
 }
